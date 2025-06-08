b9b52d86b60295890cc978f858c2dd63f78f5c75
Loui Collin-Enoch <73302977+Loui1720@users.noreply.github.com>
2025-06-08 16:07:39 +0200

    Reorderd "void setup" to be more organiced.

--- Orbito_game/JS/Chronoz_MAIN_LOOP.js
+++ Orbito_game/JS/Chronoz_MAIN_LOOP.js
@@ -3,29 +3,31 @@ function setup() {
   createCanvas(700, 700);
   background(200);
   
+  // CLASSES
   orb = new Orb(width / 2, height / 2, 10);
   score = new Score(10, 100, 125, 25);
   currentHighscore = new Score(10, 100 + 125, 125, 25);
-  currentHighscore.score = jsonData.highscore;
   target = new Target(25);
   mysteryBox = new MysteryBox();
+  mysteryBoxCounter = new Counter(1000, 1000, random(5, 10), 0, 0);
   counter = new Counter(width/2 - 750/3.65, height/2 + 750/3, 5, 750, 5);
-  timeOut = 0;
   startButton = new Button(width/2 - 115/2, height/3, 115, 55, 15, 5, 'START');
   settingsButton = new Button(width/1.25 - 115/2, 25, 170, 55, 15, 5, 'SETTINGS');
   exitSettingsButton = new Button(width/1.25 - 115/2, 25, 85, 55, 15, 5, 'EXIT');
   settings = new Settings(width/1.5, 0, width, height, color(200));
   tryAgainButton = new Button(width/2 - 145/2, height/3, 145, 55, 15, 5, 'Try Again');
+
+  // START VALUES
+  currentHighscore.score = jsonData.highscore;
+  timeOut = 0;
   
   // STATE
   state = 'START_MENU';
   settingsState = false;
   mysteryBoxShow = false;
-  mysteryBoxCounter = new Counter(1000, 1000, random(5, 10), 0, 0);
   playing = false;
   finished = false;
 }
-
 
 
 
